1ffdfd98f038b29fcc59cc638c8ef5ce6a4aa7db
y0unique <jonnathaniel.raguini@transcom.com>
2025-12-04 04:19:18 +0800

    push

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- FAVICON -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap/node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/materialdesignicons.min.css">
+</head>
+<body>
+    <!-- nav -->
+    <nav class="navbar navbar-expand-lg fixed-top navbar-custom navbar-light nav-sticky">
+        <div class="container">
+            <a href="index"><img src="assets/images/codex.png"
+                style="height: 50px; width:50px; margin-right:5px;"></a>
+            <a class="navbar-brand" href="index" target="__blank">CODEX</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="mdi mdi-menu"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item" id="index">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item" id="index">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item" id="index">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item" id="index">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item" id="index">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+
+
+
+
+
+<script src="assets/js/menu.js"></script>
+<script src="assets/js/scrollspy.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
+
